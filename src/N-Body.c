83dc2781b86748eb62f4f62ab41c317df1f4b4fe
Unknown <pasqui95@hotmail.it>
2018-07-04 19:14:46 +0200

    Clear code

--- src/N-Body.c
+++ src/N-Body.c
@@ -16,40 +16,29 @@
 
 typedef struct { 				//STRUTTURA BODY
 	float x, y, z, vx, vy, vz;  //x, y, z sono le posizioni nello spazio 3D;
-	//vx, vy, vz sono le velocità sulle cordinate x,y e z
+								//vx, vy, vz sono le velocità sulle cordinate x,y e z
 } Body;
 
 //Calcolo dei valori dei bodies in maniera casuale
 void randomizeBodies(float *data, int n) {  //La funzione prende in input un array di float e il numero di bodies
 	for (int i = 0; i < n; i++) {							//Per n body, si calcolano i valori dei bodies
-		//data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;   //Allocazione degli elementi dell'array con valori casuali
-		data[i] = 1;
+		data[i] = 2.0f * (rand() / (float)RAND_MAX) - 1.0f;   //Allocazione degli elementi dell'array con valori casuali
+		//data[i] = 1;
 	}
-	printf("USCITA randomizeBodies \n");
+	//printf("USCITA randomizeBodies \n");
 
 }
 
-//Calcolo della forza di un body
-void bodyForce(Body *p, float dt, int n, int startRange, int numBodies,MPI_Datatype body_type,int rank) {       //Prende in input un body, il tempo trascorso e il numero di body
-	printf("FUNZIONE bodyForce COUNTSEND: %d  STARTRANGE: %d \n",n,startRange);
+/**Funzione bodyForce, prende come paramentri:
+ * i bodies da considerare
+ * il tempo dell'iterazione
+ * la porzione del numero di body assegnata al processore
+ * l'indice di inizio di un processore all'interno della struttura body
+ * numero totale i bodies
+ */
+void bodyForce(Body *p, float dt, int n, int startRange, int numBodies) {
 
-	printf("STRUTTURA BODY bodyForce \n");
-	for (int i = startRange; i < startRange+n; i++) {
-		printf("RANK %d bodyForce BODIES %0.3f %0.3f %0.3f  \n",rank, p[i].x, p[i].y, p[i].z);
-	}
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-
-	printf("J BODY \n");
-	for (int i = 0; i < numBodies; i++) {
-		printf("RANK %d j BODIES %0.3f %0.3f %0.3f  \n",rank, p[i].x, p[i].y, p[i].z);
-	}
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
-
-	for (int i = startRange; i < startRange + n; i++) {          //Per ogni body
+	for (int i = startRange; i < startRange + n; i++) {          //Calcolo delle forze per la porzione di bodies di un processore
 
 		float Fx = 0.0f;				   //Si inizializza a 0 la forza su x,y e z
 		float Fy = 0.0f;
@@ -78,12 +67,8 @@ void bodyForce(Body *p, float dt, int n, int startRange, int numBodies,MPI_Datat
 		p[i].vy += dt*Fy;
 		p[i].vz += dt*Fz;
 
-		printf("RANK %d USCITA bodyForce %0.3f %0.3f %0.3f \n",rank,p[i].vx,p[i].vy,p[i].vz);
 	}
-	//MPI_Allgather(p,sizeof(*p),body_type,p,sizeof(*p),body_type,MPI_COMM_WORLD);
 
-
-	//QUESTA FASE DEVE ESSERE PARALLELIZZATA CON MPI
 }
 
 int main(int argc, char* argv[]){
@@ -107,27 +92,27 @@ int main(int argc, char* argv[]){
 	/* find out number of processes */
 	MPI_Comm_size(MPI_COMM_WORLD, &nproc);
 
-	int nBodies = 30;      //numero body
-	const float dt = 0.01f; // time step (conteggia il tempo trascorso)
-	const int nIters = 5;  // simulation iterations
+	int nBodies = atoi(argv[1]);      //numero body
+	const float dt = 0.01f;  		  //tempo iterazione
+	const int nIters = atoi(argv[2]);  //numero iterazioni
 	int count = 0;
-
-	if (argc > 1)
-		nBodies = atoi(argv[1]);
 
 
 	bytes = nBodies*sizeof(Body);             //Allocazione numero di byte in base alla dimensione di Body
 	buf = (float*)malloc(sizeof(int)*bytes);			  //Allocazione array buffer di puntatori con il numero di byte dei body
 
 
-	countSend = malloc (sizeof(int)*nproc);
-	startRange = malloc (sizeof(int)*nproc);
+	countSend = malloc (sizeof(int)*nproc);				//Array che indica quanti elementi ha ogni processore
+	startRange = malloc (sizeof(int)*nproc);		    //Array che indica quale l'indice di inizio di un processore
 
 	/*---DATATYPE CREATION---*/
+	/*Viene effettuata la creazione di un Datatype per la struttura Body, perché la struttura deve essere serializzata
+	e dovrà essere inviata agli altri processori attraverso MPI*/
+
 	const int numitem=6;
 	int blocklen[6] = {1,1,1,1,1,1};
-	MPI_Datatype types[6] = {MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT};
-	MPI_Datatype body_type;
+	MPI_Datatype types[6] = {MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT, MPI_FLOAT};   //Creazione datatype con i campi della struttura Body
+	MPI_Datatype myStruct;
 	MPI_Aint     displ[6];
 
 	displ[0] = offsetof(Body, x);
@@ -137,139 +122,126 @@ int main(int argc, char* argv[]){
 	displ[4] = offsetof(Body, vy);
 	displ[5] = offsetof(Body, vz);
 
-	MPI_Type_create_struct(numitem, blocklen, displ, types, &body_type);
-
-	MPI_Datatype myStruct;
-	MPI_Type_create_resized( body_type, 0, sizeof( Body ), &myStruct );
+	MPI_Type_create_struct(numitem, blocklen, displ, types, &myStruct);
 
 	MPI_Type_commit(&myStruct);
-	/*-------------------*/
+	/*---END DATATYPE CREATION---*/
 
-	Body *p = (Body*)buf;					      //Puntatore ad un elemento Body che contiene l'array buf
+	Body *p = (Body*)buf;					      //Puntatore elemento Body che contiene l'array buf
 
 	resto = nBodies % (nproc);
-	printf("RESTO %d \n",resto);
+	//printf("RESTO %d \n",resto);
 	portion = nBodies/(nproc);
-	printf("PORTION %d \n\n",portion);
+	//printf("PORTION %d \n\n",portion);
+
+	/**Calcolo del resto da parte di tutti i processori:
+	 * ogni processore conosce qual è la porzione degli altri processori
+	 */
 
 	for(int i = 0; i < nproc; i++){
 		countSend[i] = portion;
-		printf("RESTO FOR %d \n",resto);
-		if(resto > 0){
-			countSend[i] = countSend[i] + 1;   //Alcuni processori hanno un elemento in più
+		//printf("RESTO FOR %d \n",resto);
+		if(resto > 0){						 //Se il resto è maggiore di 0, i primi processori avranno un Body in più
+			countSend[i] = countSend[i] + 1;
 			resto--;
 		}
-		printf("COUNTSEND pos %d %d \n",i,countSend[i]);
+		//printf("COUNTSEND pos %d %d \n",i,countSend[i]);
 
 		startRange[i] = count;
-		printf("startRange %d \n",startRange[i]);
+		//printf("startRange %d \n",startRange[i]);
 
 		count = count + countSend[i];
-		printf("COUNT %d \n\n",count);
+		//printf("COUNT %d \n\n",count);
 
 	}
 
-	//printf("DOPO BARRIERA \n");
 
-	//float *recvbuf = (float*)malloc(sizeof(int)*bytes);
-
-	//float recvbuf[countSend[my_rank]];                   //Buffer con il numero di elementi assegnato ad ogni proccesso
-
-	if(my_rank == 0){
-		randomizeBodies(buf, 6*nBodies); // Init pos / vel data  (calcolo dei valori dei bodies)
+	if(my_rank == 0){					 //Il master effettua l'inizializzazione della struttura Body
+		randomizeBodies(buf, 6*nBodies); //Inizializzazione attraverso l'array di float buf
 	}
 
-	printf("BARRIER PRIMA BCAST \n");
-	MPI_Bcast(buf,bytes,MPI_FLOAT,0,MPI_COMM_WORLD);
+	//printf("BARRIER PRIMA BCAST \n");
+	MPI_Bcast(buf,bytes,MPI_FLOAT,0,MPI_COMM_WORLD);    //Viene inviato a tutti i processori l'array inizializzato
 
-	printf("PRIMA SCATTER \n");
 
 
 	//MPI_Scatterv(buf,countSend,startRange,MPI_FLOAT,recvbuf,countSend[my_rank],MPI_FLOAT,0,MPI_COMM_WORLD);
 
 
-	printf("dopo SCATTER \n");
-
-
-	/*
-	for (int i = startRange[my_rank]; i < startRange[my_rank]+countSend[my_rank]; i++) {
-		printf("RANK %d BUFFF %0.3f \n",my_rank, buf[i]);
-	}*/
-
-	/*
-	for (int i = startRange[my_rank]; i < startRange[my_rank]+countSend[my_rank]; i++) {
-		printf("RANK %d BODIES %0.3f %0.3f %0.3f  \n",my_rank, p[i].x, p[i].y, p[i].z);
-	}*/
-
-	printf("\n");
-
-	MPI_Barrier(MPI_COMM_WORLD);
-
 
 	startTime = MPI_Wtime();
 
+
+	/*
 	Body *bodies = (Body*)buf;
 
 	for (int i = startRange[my_rank]; i < startRange[my_rank]+countSend[my_rank]; i++) {
 		printf("RANK %d BODIES %0.3f %0.3f %0.3f  \n",my_rank, bodies[i].x, bodies[i].y, bodies[i].z);
-	}
+	}*/
+
+
 
 	for (int iter = 1; iter <= nIters; iter++) {              //INIZIO SIMULAZIONE
 
-		printf("ITER %d PROCESSOR %d \n",iter,my_rank);
+		printf("ITER %d PROCESSORE %d  Indice di inizio %d Num.particelle da calcolare %d \n",iter,my_rank,startRange[my_rank],countSend[my_rank]);
 
-		bodyForce(p,dt,countSend[my_rank],startRange[my_rank],nBodies,body_type,my_rank);
+		//Calcolo della forza dei Body da parte di un processore
+		bodyForce(p,dt,countSend[my_rank],startRange[my_rank],nBodies);
 
 
-		//CALCOLO DELLE POSIZIONI x,y,z dei bodies
-		for (int i = startRange[my_rank] ; i < startRange[my_rank] + countSend[my_rank]; i++) { // integrate position
-			//Si calcola, velocità su x per dt(tempo trascorso) e il risultato viene sommato alla posizione x
+		//CALCOLO DELLE POSIZIONI x,y,z della porzione di bodies di un processore
+		for (int i = startRange[my_rank] ; i < startRange[my_rank] + countSend[my_rank]; i++) {
 			p[i].x += p[i].vx*dt;
 			p[i].y += p[i].vy*dt;
 			p[i].z += p[i].vz*dt;
 		}
 
+		/*
 		printf("PRIMA ALLGATHER \n");
 		for (int i = 0; i < nBodies; i++) {
 			printf("RANK %d BODIES n %d %0.3f %0.3f %0.3f  \n",my_rank,i, p[i].x, p[i].y, p[i].z);
-		}
+		}*(
 
-
+		//Viene inviata a tutti i processori la porzione di bodies modificati da ogni processore, ogni processore conosce le posizioni e la forze dei bodies calcolate dagli altri proce
+		/**
+		 * Vengono utilizzati i puntatori countSend e startRange, perché ogni processore conosce qual è la porzione assegnata agli altri processori
+		 * Utilizzando la Allgatherv, viene inviata ad ogni procesore la porzione calcolata da un processore
+		 * In questo modo ogni processore conosce le posizioni e le forze dei bodies calcolati dagli altri processori
+		 */
 		MPI_Allgatherv(MPI_IN_PLACE,0,myStruct,p,countSend,startRange,myStruct,MPI_COMM_WORLD);
 
+		/*
 		printf("DOPO ALLGATHER \n");
 		for (int i = 0; i < nBodies; i++) {
 			printf("RANK %d BODIES n.%d %0.3f %0.3f %0.3f  \n",my_rank,i, p[i].x, p[i].y, p[i].z);
-		}
-
-		MPI_Barrier(MPI_COMM_WORLD);
+		}*/
 
 	}
 
 
 
-	MPI_Barrier(MPI_COMM_WORLD);
-	//printf("DOPO BARRIERA \n");
+	MPI_Barrier(MPI_COMM_WORLD); //Si attende che tutti processori abbiano completato la simulazione
 
 	endTime = MPI_Wtime();
 
-
-
+	//Il master stampa il valore finale dei bodies e il tempo della simulazione
 	if(my_rank == 0){
-		printf("\nTEMPO TRASCORSO %f \n",endTime - startTime);
 		for(int i=0; i < nBodies; i++){
 			printf("BODY n: %d X:%0.3f Y:%0.3f Z:%0.3f VX:%0.3f VY:%0.3f VZ:%0.3f \n",i,p[i].x,p[i].y,p[i].z,p[i].vx,p[i].vy,p[i].vz);
 		}
+		printf("\nTEMPO TRASCORSO %f \n",endTime - startTime);
 
 	}
 
 
 	/* shut down MPI */
-	MPI_Type_free(&body_type);
+	MPI_Type_free(&myStruct);    //Viene liberata la memoria allocata per il datatype creato
 
+	//Vengono deallocati i puntatori
 	free(buf);
 	free(countSend);
 	free(startRange);
+
 	MPI_Finalize();
 
 	return 0;
